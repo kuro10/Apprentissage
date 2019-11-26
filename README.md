90f5a60ac2d90623bd84ec62fcda65c236d43df2
kuro10 <trantronghieu97@gmail.com>
2019-11-26 22:03:50 +0100

    Update TP1_prog1.py

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Apprentissage
 INSA - 5SDBD - TP Apprentissage
+
+
+## Todo 
+
+- [x] TP1_prog1.py
+
+- [ ] TP1_prog2.py
+
+- [ ] TP1.ipynb 
+
+- [ ] TP2_prog1.py
+
+- [ ] TP2.ipynb
+
+- [ ] TP3_prog1.py
+
+- [ ] TP3.ipynb
+
--- TP1/TP1_prog1.py
+++ TP1/TP1_prog1.py
@@ -4,20 +4,43 @@ import matplotlib.pyplot as plt
 import argparse
 from sklearn.datasets import fetch_openml
 
-
-def main(): 
-    ds = fetch_openml(DATA_NAME)
-    print(ds)
+def load_data(dataname):
+    ds = fetch_openml(dataname)
+    print(ds.DESCR)
+    print("============================")
     print("Shape of data : ", ds.data.shape)
     print("Shape of target : ",ds.target.shape)
+    return ds.data, ds.target
 
+
+def main(): 
+    data, target = load_data(DATA_NAME)
+    target_list = np.unique(target)
+    print("List of targets : ", target_list)
+
+    images = data.reshape((-1,28,28))
+    print("======= Plot random data ==========")
+
+    plt.figure(figsize=(12,8))
+    for i in range(10):
+        idx = np.random.randint(len(data))
+        plt.subplot(2,5,i+1)
+        plt.imshow(images[idx],cmap=plt.cm.gray_r,interpolation="nearest")
+        plt.title("Target : {}".format(target[idx]))
+    plt.show()  
+    plt.savefig("random-training-data.png")
+    print("Image saved as random-training-data.png")
+    
 
 if __name__ == '__main__':
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--dataset", required=False, default='mnist_784',\
-                help="name of dataset from openML")
+    ap.add_argument("-d", "--dataset", required=False,  default='mnist_784',\
+                help="name of dataset from openML, type = string (available here : 'mnist_784', 'Fashion-MNIST')")
     args = vars(ap.parse_args())
-    
+    print("[DOC] -h for help")
     DATA_NAME = args['dataset']
+    print("Explore dataset", DATA_NAME)
+    print("===================================")
     main()
+    print("=============End===================")
