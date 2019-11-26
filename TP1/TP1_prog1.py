1a44568144adc596b5f1038f30fb4015cd3cbd2d
Kuro <trantronghieu97@gmail.com>
2019-11-26 21:09:55 +0100

    Update TP1

--- TP1/TP1_prog1.py
+++ TP1/TP1_prog1.py
@@ -1,32 +1,23 @@
 # import library
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_mldata
-
-# import data
+import argparse
 from sklearn.datasets import fetch_openml
 
 
-# create target_list
-target_list = np.unique(mnist.target)
-print(target_list)
-
-def visualize_data(dataset): 
-    print("Shape of data : ", dataset.data.shape)
-    print("Shape of target : ",dataset.target.shape)
-    target_list = np.unique(dataset.target)
-    print(target_list)
-    plt.figure(figsize=(12,8))
-    for i in range(10):
-        idx = np.random.randint(len(dataset.data))
-        plt.subplot(2,5,i+1)
-        plt.imshow(dataset.data[idx].reshape(28,28))
-        plt.title("Target : {}".format(dataset.target[idx]))
-    plt.show()
-
 def main(): 
-   mnist = fetch_openml('mnist_784')
-   visualize_data(mnist)
+    ds = fetch_openml(DATA_NAME)
+    print(ds)
+    print("Shape of data : ", ds.data.shape)
+    print("Shape of target : ",ds.target.shape)
 
 
-
+if __name__ == '__main__':
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required=False, default='mnist_784',\
+                help="name of dataset from openML")
+    args = vars(ap.parse_args())
+    
+    DATA_NAME = args['dataset']
+    main()
